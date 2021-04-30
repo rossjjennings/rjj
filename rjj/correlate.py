95df9f4c1e62f04f12820ea05539af3459e9804e
Ross Jennings <rossjjennings@gmail.com>
2021-04-29 19:24:43 -0500

    Add numba and stride_tricks variants of corr_2pt()

--- rjj/correlate.py
+++ rjj/correlate.py
@@ -1,4 +1,17 @@
 import numpy as np
+from np.lib.stride_tricks import as_strided
+import numba
+
+def corr_2pt_naive(arr1, arr2):
+    m, n = arr1.size, arr2.size
+    out = np.zeros(m + n - 1)
+    for k in range(m + n - 1):
+        start1 = max(0, k - n + 1)
+        start2 = max(0, n - k - 1)
+        len = min(k + 1, m) - start1
+        for i in range(len):
+            out[k] += arr1[start1 + i]*arr2[start2 + i]
+    return out
 
 def corr_2pt(arr1, arr2):
     '''
@@ -13,6 +26,26 @@ def corr_2pt(arr1, arr2):
         max1 = min(k + 1, m)
         out.append(np.sum(arr1[min1:max1] * arr2[min2:max2]))
     return np.array(out)
+
+def corr_2pt_insane(arr1, arr2):
+    m, n = len(arr1), len(arr2)
+    arr1_ext = np.zeros(m + 2*n - 2)
+    arr1_ext[n - 1 : m + n - 1] = arr1
+    s = arr1.strides[0]
+    INSANITY = as_strided(arr1_ext, shape=(m + n - 1, n), strides=(s, s))
+    return np.sum(INSANITY * arr2, axis=-1)
+
+@numba.jit
+def corr_2pt_numba(arr1, arr2):
+    m, n = arr1.size, arr2.size
+    out = np.zeros(m + n - 1)
+    for k in range(m + n - 1):
+        start1 = max(0, k - n + 1)
+        start2 = max(0, n - k - 1)
+        len = min(k + 1, m) - start1
+        for i in range(len):
+            out[k] += arr1[start1 + i]*arr2[start2 + i]
+    return out
 
 def intermediates(arr1, arr2):
     '''
