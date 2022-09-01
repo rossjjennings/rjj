c5040f42daa7db3496cb2b4d2457d3888e966215
Ross Jennings <rossjjennings@gmail.com>
2022-09-01 19:07:09 -0400

    Use legend-like options

--- rjj/plotting.py
+++ rjj/plotting.py
@@ -1,14 +1,36 @@
 import matplotlib as mpl
 
-def anchored_text(ax, text, loc, style="square"):
+def anchored_text(ax, text, loc, fancybox=None, edgecolor=None, facecolor=None,
+                  framealpha=None, frameon=None):
+    if fancybox is None:
+        fancybox = mpl.rcParams["legend.fancybox"] # default: True
+
+    if facecolor is None:
+        facecolor = mpl.rcParams["legend.facecolor"] # default: "inherit"
+    if facecolor == "inherit":
+        facecolor = mpl.rcParams["axes.facecolor"] # default: "white"
+
+    if edgecolor is None:
+        edgecolor = mpl.rcParams["legend.edgecolor"] # default: "0.8"
+    if edgecolor == "inherit":
+        edgecolor = mpl.rcParams["axes.edgecolor"]
+
+    if framealpha is None:
+        framealpha = mpl.rcParams["legend.framealpha"] # default: 0.8
+
+    if frameon is None:
+        frameon = mpl.rcParams["legend.frameon"] # default: True
+
     textbox = mpl.offsetbox.AnchoredText(text, loc)
-    if style == "square":
-        textbox.patch.set_boxstyle("square")
-    elif style == "round":
+
+    if fancybox:
         textbox.patch.set_boxstyle("round", pad=0, rounding_size=0.2)
-    elif style == "legend":
-        textbox.patch.set_boxstyle("round", pad=0, rounding_size=0.2)
-        textbox.patch.set_facecolor((1.0, 1.0, 1.0, 0.8))
-        textbox.patch.set_edgecolor((0.8, 0.8, 0.8, 0.8))
+    else:
+        textbox.patch.set_boxstyle("square", pad=0)
+    textbox.patch.set_facecolor(facecolor)
+    textbox.patch.set_edgecolor(edgecolor)
+    textbox.patch.set_alpha(framealpha)
+    textbox.patch.set_visible(frameon)
+
     ax.add_artist(textbox)
     return textbox
